c3e4818dc866245db9af7834954a2b32849c4be0
NMarcell01 <112898634+NMarcell01@users.noreply.github.com>
2023-03-27 12:43:31 +0200

    Add files via upload

---
+++ szamologepjs.js
@@ -0,0 +1,68 @@
+function hetes() {
+    document.szamologep.kijelzo.value+=7;
+}
+
+function nyolcas() {
+    document.szamologep.kijelzo.value+=8;
+}
+
+function kilences() {
+    document.szamologep.kijelzo.value+=9;
+}
+
+
+function plusz() {
+    document.szamologep.kijelzo.value+="+";
+}
+
+function negyes() {
+    document.szamologep.kijelzo.value+=4;
+}
+
+function otos() {
+    document.szamologep.kijelzo.value+=5;
+}
+
+function hatos() {
+    document.szamologep.kijelzo.value+=6;
+}
+
+function minusz() {
+    document.szamologep.kijelzo.value+="-";
+}
+
+function egyes() {
+    document.szamologep.kijelzo.value+=1;
+}
+
+function kettes() {
+    document.szamologep.kijelzo.value+=2;
+}
+
+function harmas() {
+    document.szamologep.kijelzo.value+=3;
+}
+
+function szorzas() {
+    document.szamologep.kijelzo.value+="*";
+}
+
+
+function nullas() {
+    document.szamologep.kijelzo.value+=0;
+}
+
+function osztas() {
+    document.szamologep.kijelzo.value+="/";
+}
+
+
+
+function egyenlo() {
+    eredmeny=eval(document.szamologep.kijelzo.value);
+    document.szamologep.kijelzo.value=eredmeny;
+}
+
+
+
+
